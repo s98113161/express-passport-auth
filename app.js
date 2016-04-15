2637c4bc8fbae32e52f563d2f38b6802fd1c4026
Hector Kang <s98113161@stu.edu.tw>
2016-04-15 14:22:56 +0800

    make it better

---
+++ app.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var mongoose = require('mongoose');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var session= require('express-session');
+var passport = require('passport');
+var DBconfig = require('./config/database.js');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//connect db
+mongoose.connect(DBconfig.url);
+
+require('./config/passport')(passport); // pass passport for configuration
+
+app.use(logger('dev')); // log every request to the console
+app.use(bodyParser());  //處理POST的body能力
+app.use(bodyParser.urlencoded({ extended: false }));//處理QueryString 
+app.use(cookieParser());//cookie
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
+
+require('./routes/index.js')(app,passport);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+// error handlers
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+var port = process.env.PORT || '3000';
+app.listen(port);
+console.log('The server is create on port ' + port);
+//module.exports = app;
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://kangyozia:123456@ds023490.mlab.com:23490/my-first-express-db' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+ 
+};
